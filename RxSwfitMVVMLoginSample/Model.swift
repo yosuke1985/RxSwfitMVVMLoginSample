e6597dc01801422d6db0f5f0bd3cfb3d164d9911
yosuke1985 <ihatenonoildressing@gmail.com>
2020-01-03 21:24:47 +0900

    Modelの追加。 エラーの定義とID, Passの値の有無かつ空文字かどうかによってエラーを振り分けるvalidateを定義した。

---
+++ RxSwfitMVVMLoginSample/Model.swift
@@ -0,0 +1,57 @@
+//
+//  Model.swift
+//  RxSwfitMVVMLoginSample
+//
+//  Created by 中山 陽介 on 2020/01/03.
+//  Copyright © 2020 Yosuke Nakayama. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+enum ModelError: Error {
+    case invalidId
+    case invalidPassword
+    case invalidIdAndPassword
+}
+
+extension ModelError {
+    var errorText: String {
+        switch self {
+        case .invalidIdAndPassword:
+            return "IDとPasswordが未入力です。"
+        case .invalidId:
+            return "IDが未入力です。"
+        case .invalidPassword:
+            return "Passwordが未入力です。"
+        }
+    }
+}
+
+protocol ModelProtocol {
+    func validate(idText:String?, passwordText:String?)->Observable<Void>
+}
+
+class Model: ModelProtocol {
+    func validate(idText: String?, passwordText: String?) -> Observable<Void> {
+        switch (idText, passwordText) {
+        case (.some, .none):
+            return Observable.error(ModelError.invalidPassword)
+        case (.none, .some):
+            return Observable.error(ModelError.invalidId)
+        case (.none, .none):
+            return Observable.error(ModelError.invalidIdAndPassword)
+        case (let idText?, let passwordText?):
+            switch (idText.isEmpty, passwordText.isEmpty) {
+            case (true, true):
+                return Observable.error(ModelError.invalidIdAndPassword)
+            case (true, false):
+                return Observable.error(ModelError.invalidPassword)
+            case (false, true):
+                return Observable.error(ModelError.invalidId)
+            case (false, false):
+                return Observable.just(())
+            }
+        }
+    }
+}
