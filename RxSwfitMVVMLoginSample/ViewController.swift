54b5fd4816a2354752be5738a4257d39700b0608
yosuke1985 <ihatenonoildressing@gmail.com>
2020-01-03 20:53:45 +0900

    Viewの完成。 StoryBoardに配置したID, PassのTextFieldとvalidationの結果を表示するためのラベル

--- RxSwfitMVVMLoginSample/ViewController.swift
+++ RxSwfitMVVMLoginSample/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var validationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
